25de5159b1f5bc73706281e2f84473444f509907
navodmeethal <navodm@ducont.com>
2023-12-23 14:00:35 +0400

    authguard

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -17,8 +17,6 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class ApiService {
- 
-
   baseURL = environment.baseURL;
   authTokenURL = environment.authTokenURL;
   //sauthTokenURL="https://mofastg.mofaic.gov.ae/en/Account/"
@@ -38,7 +36,7 @@ export class ApiService {
       .post(this.baseURL + servicename, data)
       .pipe(catchError(this.handleError));
   }
-  postWH(servicename: any, data: any, header:any) {
+  postWH(servicename: any, data: any, header: any) {
     return this.http
       .post(this.baseURL + servicename, data, header)
       .pipe(catchError(this.handleError));
@@ -46,14 +44,14 @@ export class ApiService {
   sPassAuthGetUserprofile(param1: any, param3: any) {
     const email = encodeURIComponent(param3.trim());
     let email2 = encodeURIComponent(email);
-    
+
     console.log(email);
     console.log(email2);
 
     let Url =
       this.baseURL +
       `Common/CheckUAEPassLogin?sAuthCode=${param1}&sEmail=${email2}`;
-      console.log(Url)
+    console.log(Url);
 
     return this.http.get(Url, this.headerOptions);
   }
@@ -115,20 +113,18 @@ export class ApiService {
   }
 
   postXML(serviceUrl: any, soapRequest: any) {
-  //  debugger;
     const headers = new HttpHeaders({
       'Content-Type': 'text/xml; charset=utf-8',
-      'SOAPAction': 'Sign',
-      'TwsAuthN': 'urn:safelayer:tws:policies:authentication:oauth:clients'
+      SOAPAction: 'Sign',
+      TwsAuthN: 'urn:safelayer:tws:policies:authentication:oauth:clients',
     });
 
     return this.http
       .post(serviceUrl, soapRequest, { headers, responseType: 'text' })
       .pipe(catchError(this.handleError));
-
   }
 
-  loadarabicval(){
+  loadarabicval() {
     return this.http.get('http://localhost:3000/txnlog');
-    }
+  }
 }
--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -3,12 +3,10 @@ import { BehaviorSubject } from 'rxjs';
 import { Subject, Observable } from 'rxjs';
 import { Router } from '@angular/router';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   favink1: string = '';
   favink2: string = '';
   public userloggedinSubject = new BehaviorSubject<boolean>(false);
@@ -35,174 +33,148 @@ export class AuthService {
   private selectedcompany = new BehaviorSubject<string>('');
   private freeZone = new BehaviorSubject<string>('');
 
-
   private userRoleSubject = new Subject<string>();
   userRole$ = this.userRoleSubject.asObservable();
-  userRole: string=''; // You might want to initialize this with a default value if needed
-
+  userRole: string = ''; // You might want to initialize this with a default value if needed
 
   private inactivityTimer: any;
   private readonly INACTIVITY_TIMEOUT = 15 * 60 * 1000; // 15 minutes in milliseconds
-  isSidebar:boolean=false
+  isSidebar: boolean = false;
 
-  constructor(private router:Router) { 
-
-    let data2=sessionStorage.getItem('userProfile');
-    if(data2!=undefined || data2 !=null){
-            let abc=JSON.parse(data2)
-            this.userprofilesubject.next(abc.Data?.firstnameEN);
-            let usertypedata=this.getLCAUser() || '';
-            console.log(usertypedata)
-            if(usertypedata){
-       this.lcauserloggedinSubject.next(true)
-            }
-            else{
-             this.lcauserloggedinSubject.next(false)
-            }
-    }
-    else{
-      this.lcauserloggedinSubject.next(false)
-    
+  constructor(private router: Router) {
+    let data2 = sessionStorage.getItem('userProfile');
+    if (data2 != undefined || data2 != null) {
+      let abc = JSON.parse(data2);
+      this.userprofilesubject.next(abc.Data?.firstnameEN);
+      let usertypedata = this.getLCAUser() || '';
+      console.log(usertypedata);
+      if (usertypedata) {
+        this.lcauserloggedinSubject.next(true);
+      } else {
+        this.lcauserloggedinSubject.next(false);
+      }
+    } else {
+      this.lcauserloggedinSubject.next(false);
     }
 
-    
-    let data:any;
-    data=sessionStorage.getItem('currentcompany');
+    let data: any;
+    data = sessionStorage.getItem('currentcompany');
     console.log(data);
 
-    if(data!=undefined || data !=null){
+    if (data != undefined || data != null) {
       this.userloggedinSubject.next(true);
-            let abc=JSON.parse(data)
-            abc=abc.role;
-            console.log(data)
-            this.userCompanysubject.next(data.business_name)
+      let abc = JSON.parse(data);
+      abc = abc.role;
+      console.log(data);
+      this.userCompanysubject.next(data.business_name);
 
-            if(abc=="Admin"){
-            this.isAdmin.next(true);
-            }
-            else if(abc=="User"){
-            this.isAdmin.next(false);
-            }
-            else{
-              this.isAdmin.next(false);
-            }
+      if (abc == 'Admin') {
+        this.isAdmin.next(true);
+      } else if (abc == 'User') {
+        this.isAdmin.next(false);
+      } else {
+        this.isAdmin.next(false);
       }
-      else{
+    } else {
       this.userloggedinSubject.next(false);
       this.userCompanysubject.next('');
-      }
-    
-
+    }
   }
 
-
-  logout(){
-this.userloggedinSubject.next(false);
-    this.lcauserloggedinSubject.next(false)
+  logout() {
+    this.userloggedinSubject.next(false);
+    this.lcauserloggedinSubject.next(false);
     sessionStorage.clear();
     localStorage.clear();
-    this.router.navigateByUrl('/logout')
-
+    this.router.navigateByUrl('/logout');
   }
 
   getLCAUser(): string {
     // Retrieve user role from localStorage
     return sessionStorage.getItem('userrole') || '';
-    
   }
-
-  
 
   setLCAUser(role: string): void {
     this.userRole = role;
     // this.userRoleSubject.next(role);
-this.lcauserloggedinSubject.next(true)
+    this.lcauserloggedinSubject.next(true);
 
-sessionStorage.setItem('userrole', role);
+    sessionStorage.setItem('userrole', role);
     this.userRoleSubject.next(role);
   }
-  
-   getUserProfile() {
+
+  getUserProfile() {
     const userProfileString = sessionStorage.getItem('userProfile');
     if (userProfileString) {
-      let abc=JSON.parse(userProfileString);
-      let abc1=JSON.parse(abc);
+      let abc = JSON.parse(userProfileString);
+      let abc1 = JSON.parse(abc);
 
       console.log(abc1);
       return JSON.parse(userProfileString);
-    }
-    else{
-    return null
+    } else {
+      return null;
     }
   }
-   setUserProfile(userProfile: any) {
+  setUserProfile(userProfile: any) {
     console.log(userProfile);
     sessionStorage.setItem('userProfile', JSON.stringify(userProfile));
-    let userdata=JSON.parse(userProfile)
+    let userdata = JSON.parse(userProfile);
 
     //this.userprofilesubject.next(userdata);
 
-    if(userdata!=undefined || userdata !=null){
-            // let abc=JSON.parse(userdata)
-            this.userprofilesubject.next(userdata.Data?.firstnameEN);
+    if (userdata != undefined || userdata != null) {
+      // let abc=JSON.parse(userdata)
+      this.userprofilesubject.next(userdata.Data?.firstnameEN);
     }
   }
-  
-  getSelectedCompany() {
-     const myselectedcompany = sessionStorage.getItem('currentcompany');
-      if (myselectedcompany) {
-       return JSON.parse(myselectedcompany);
-     }
-     else{
-      let dat=sessionStorage.getItem('userProfile');
 
-      if(dat!=undefined ||dat!=null )
-      {
-        console.log("to landing page from common service line 284")
+  getSelectedCompany() {
+    const myselectedcompany = sessionStorage.getItem('currentcompany');
+    if (myselectedcompany) {
+      return JSON.parse(myselectedcompany);
+    } else {
+      let dat = sessionStorage.getItem('userProfile');
+
+      if (dat != undefined || dat != null) {
+        console.log('to landing page from common service line 284');
         this.router.navigateByUrl('/landingpage');
       }
       // else{
       //   //this.logout();//vincy
       //   this.redirecttoEdas();
       // }
-     }
-     //return null;
-   }
-   redirecttoEdas(){
-    window.location.href = "https://stg-id.uaepass.ae/idshub/logout?redirect_uri=https://mofastg.mofaic.gov.ae/en/Account/Redirect-To-EDAS-V2"
+    }
+    //return null;
+  }
+  redirecttoEdas() {
+    window.location.href =
+      'https://stg-id.uaepass.ae/idshub/logout?redirect_uri=https://mofastg.mofaic.gov.ae/en/Account/Redirect-To-EDAS-V2';
   }
 
-
-   
- setSelectedCompany(data: any) {
-     sessionStorage.setItem('currentcompany', JSON.stringify(data));
-     if(data!=undefined || data !=null){
+  setSelectedCompany(data: any) {
+    sessionStorage.setItem('currentcompany', JSON.stringify(data));
+    if (data != undefined || data != null) {
       this.userloggedinSubject.next(true);
-      this.userCompanysubject.next(data.business_name)
-          let  abc=data.role;
-            if(abc=="Admin"){
-            this.isAdmin.next(true);
-            }
-            else if(abc=="User"){
-            this.isAdmin.next(false);
-            }
-            else{
-            this.isAdmin.next(false);
-            }
+      this.userCompanysubject.next(data.business_name);
+      let abc = data.role;
+      if (abc == 'Admin') {
+        this.isAdmin.next(true);
+      } else if (abc == 'User') {
+        this.isAdmin.next(false);
+      } else {
+        this.isAdmin.next(false);
       }
-      else{
-     // this.userloggedin=false;
+    } else {
+      // this.userloggedin=false;
       this.userloggedinSubject.next(false);
-      this.userCompanysubject.next('')
-      }
-   }
+      this.userCompanysubject.next('');
+    }
+  }
 
-
-
-   isAuthenticated(): boolean {
+  isAuthenticated(): boolean {
     const userProfile = this.getUserProfile();
     const selectedCompany = this.getSelectedCompany();
-    const isLCUser=this.getLCAUser();
+    const isLCUser = this.getLCAUser();
 
     // Check if both user profile and selected company are available
     if (userProfile && selectedCompany) {
@@ -222,14 +194,11 @@ sessionStorage.setItem('userrole', role);
       // Add more conditions as needed based on your specific requirements
       // If no specific conditions are met, return false
       return false;
-    } 
-    else if(userProfile){
-      return true;   // needd to correct this
-    }
-    else {
+    } else if (userProfile) {
+      return true; // needd to correct this
+    } else {
       // If either user profile or selected company is missing, return false
       return false;
     }
   }
-
 }
